dc6d0d4999093336db0a7fcb2b7e7a2d2d98e983
Rafael Freitas <raffreitasx@gmail.com>
2024-12-31 12:57:10 -0300

    feat: implement initial database setup with Product entity and migrations

---
+++ src/Orders/Ecomm.Orders.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Ecomm.Orders.Domain.Primitives;
+
+namespace Ecomm.Orders.Domain.Entities;
+
+public class Product : Entity
+{
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public decimal Price { get; init; }
+    public string ImageUrl { get; init; }
+
+    public Product(string name, string description, decimal price, string imageUrl)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+        ImageUrl = imageUrl;
+    }
+}
