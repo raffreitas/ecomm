51d4df2dd3a77e228bc14cbb7051fd5f8c93af6f
Rafael Freitas <raffreitasx@gmail.com>
2024-12-31 14:55:36 -0300

    feat: add FluentValidation for CreateOrder command and implement validation logic

---
+++ src/Orders/Ecomm.Orders.Application/Orders/CreateOrder/CreateOrderCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Ecomm.Orders.Application.Orders.CreateOrder;
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.CardHash).NotEmpty();
+        RuleForEach(x => x.Items)
+            .ChildRules(x =>
+            {
+                x.RuleFor(item => item.Quantity).NotEmpty().GreaterThan(0);
+                x.RuleFor(item => item.ProductId).NotEmpty().NotEqual(Guid.Empty);
+            });
+    }
+}
