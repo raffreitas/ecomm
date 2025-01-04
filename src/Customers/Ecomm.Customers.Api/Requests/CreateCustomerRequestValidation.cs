17e7c397f7495cfc58b79d419cb24fc84d0e5f74
Rafael Freitas <raffreitasx@outlook.com>
2025-01-04 18:23:43 -0300

    feat: implement customer management features with CRUD operations and message bus integration

---
+++ src/Customers/Ecomm.Customers.Api/Requests/CreateCustomerRequestValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Ecomm.Customers.Api.Requests;
+
+public class CreateCustomerRequestValidation : AbstractValidator<CreateCustomerRequest>
+{
+    public CreateCustomerRequestValidation()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+    }
+}
---
+++ src/Customers/Ecomm.Customers.Api/Services/ICustomerService.cs
@@ -0,0 +1,8 @@
+﻿using Ecomm.Customers.Api.Requests;
+
+namespace Ecomm.Customers.Api.Services;
+
+public interface ICustomerService
+{
+    Task CreateAsync(CreateCustomerRequest customerRequest, CancellationToken cancellationToken = default);
+}
